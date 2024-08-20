f332968b82b56cfa9d745ff2393fe2bc26cf6b4d
Shubham Kumar  Singh <91650336+chitshubham@users.noreply.github.com>
2024-08-20 11:36:32 +0530

    Time: 11 ms (44.19%), Space: 7.4 MB (77.36%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
+   int solve(int currA, int clipA, int n ) 
+   {
+     if(currA == n) 
+     {
+         return 0;
+     }
+     if(currA>n) 
+     {
+         return 100000;
+     }
+     int paste = 1+solve(currA+clipA,clipA,n);
+     int copyAllpaste = 1+1+solve(currA+currA,currA,n);
+     return min(paste,copyAllpaste);
+   }
     int minSteps(int n) {
-        if (n == 1) return 0;
-        
-        int steps = 0;
-        int factor = 2;
-        
-        while (n > 1) {
-            while (n % factor == 0) {
-                steps += factor;
-                n /= factor;
-            }
-            factor++;
+        if(n==1)
+        {
+             return 0;
         }
-        
-        return steps;
+        return 1+ solve(1,1,n);
     }
 };
