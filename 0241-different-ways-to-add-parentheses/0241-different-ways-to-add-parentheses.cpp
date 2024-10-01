b6c0a2d66db2d663eb3a8fb9cb3d21a921bdf147
Shubham Kumar  Singh <91650336+chitshubham@users.noreply.github.com>
2024-10-02 00:57:42 +0530

    Time: 4 ms (44.96%), Space: 13.8 MB (49.99%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    vector<int>solve(string s){
-        vector<int>result;
-         for(int i = 0; i<s.length();++i){
-            if(s[i]=='+'|| s[i]=='-'  || s[i]=='*'){
-                vector<int>left =solve(s.substr(0,i));
-                vector<int>right = solve(s.substr(i+1));
-                for( int& x : left){
-                    for(int & y : right){
-                        if(s[i]=='+') result.push_back(x+y);
-                        else if(s[i]=='-') result.push_back(x-y);
-                        else result.push_back(x*y);
-                    }
+  vector<int>solve(string s ) {
+     vector<int>res;
+     for( int i = 0;i<s.length();++i) {
+        if(s[i]=='+' || s[i]=='-' || s[i]=='*' ){
+            vector<int>left = solve(s.substr(0,i));
+            vector<int>right = solve(s.substr(i+1));
+             for(int & x : left ){
+                for(int & y : right){
+                    if(s[i]=='+') res.push_back(x+y);
+                    else if(s[i]=='-') res.push_back(x-y);
+                    else res.push_back(x*y);
                 }
-             }             
-         }
-         if(result.empty()) result.push_back(stoi(s));
-        return result;
-    }
+             }
+        }
+     }
+     if(res.empty()) res.push_back(stoi(s));
+     return res; 
+  }
     vector<int> diffWaysToCompute(string expression) {
         return solve(expression);
     }
