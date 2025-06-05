abfbac1220808ce4be6a0c251c347af3371ee321
Shubham Kumar  Singh <91650336+chitshubham@users.noreply.github.com>
2025-06-05 12:34:27 +0530

    Time: 0 ms (100%), Space: 8.7 MB (33.7%) - LeetHub

--- 0306-additive-number/0306-additive-number.cpp
+++ 0306-additive-number/0306-additive-number.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    bool check(const  string& str1 ,const string& str2 ,const string& rem){
-      if ((str1.size() > 1 && str1[0] == '0') || (str2.size() > 1 && str2[0] == '0')){
-         return false;
-      }
-        string sum = to_string(stol(str1) + stol(str2));
-        int sumSize= sum.size();
+    bool check(string first , string second , string rem){
+        if((first.size()>1 && first[0]=='0') || (second.size()>1 && second[0]=='0')) return false;
+        string sum = to_string(stol(first)+stol(second));
         if(sum==rem) return true;
-        else if(sum.size()>=rem.size() || rem.substr(0,sumSize)!=sum) return false;
-        return check(str2,sum,rem.substr(sumSize));
-        }
+        int sumSize = sum.size();
+        if(sumSize>=rem.size() || rem.substr(0,sumSize)!= sum) return false;
+        return check(second,sum,rem.substr(sumSize));
+    }
     bool isAdditiveNumber(string num) {
-        int n = num.size();
-        for(int i =1;i<=n/2;++i){
-            for(int j =1;j<=(n -i)/2;++j){
-                if(check(num.substr(0,i),num.substr(i,j),num.substr(i+j))) return true;
-            }
+       int n = num.size();
+       for(int i =1;i<=n/2;++i){
+        for(int j =1;j<=(n-i)/2;++j){
+            if(check(num.substr(0,i),num.substr(i,j),num.substr(i+j))) return true;
         }
-        return false;
+       }
+       return false;
     }
 };
